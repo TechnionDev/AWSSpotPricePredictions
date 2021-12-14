4e3e0aaab3878611336f77c374e222305859ea02
Gur Telem <gtelem@apple.com>
2021-12-14 23:35:26 +0200

    Scripts enhancements

--- get_int_json.py
+++ get_int_json.py
@@ -5,10 +5,11 @@ def main():
     files = os.listdir('./data')
     files.sort()
     files = [f for f in files if f.startswith('spot-advisor-data_') and f.endswith('.json')]
-    print(f'files: {files}')
+    print(f'num of files: {len(files)}')
 
     if len(files) < 2:
-        return
+        print(f'Not enough files')
+        exit(0)
 
     with open(os.path.join('data', files[-2]), 'r') as f:
         prev_content = f.read()
@@ -17,8 +18,12 @@ def main():
         curr_content = f.read()
 
     if prev_content == curr_content:
+        print('Last two files are dup. Deleteing last one')
         os.remove(os.path.join('data', files[-1]))
+    else:
+        exit(0)
 
 
 if __name__ == '__main__':
-    main()
+    for _ in range(30):
+	    main()
